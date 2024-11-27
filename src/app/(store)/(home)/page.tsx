f9ccec406938bf5ec8fe781d0b2bfdf549675e81
Hamasaki <felipehama@gmail.com>
2024-11-27 16:56:49 -0300

    Initial commit

---
+++ src/app/(store)/(home)/page.tsx
@@ -0,0 +1,75 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
+import Image from 'next/image'
+import Link from 'next/link'
+
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured')
+  const products = await response.json()
+  return products
+}
+
+export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
+
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href={`/products/${highlightedProduct.slug}`}
+        className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src={highlightedProduct.image}
+          alt=""
+          className="transition-transform duration-300 group-hover:scale-105"
+          width={1200}
+          height={100}
+          quality={100}
+        />
+
+        <div className="absolute right-28 bottom-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">{highlightedProduct.title}</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            {highlightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0
+            })}
+          </span>
+        </div>
+      </Link>
+
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/products/${product.slug}`}
+            className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+          >
+            <Image
+              src={product.image}
+              alt=""
+              className="transition-transform duration-300 group-hover:scale-105"
+              width={1000}
+              height={1000}
+              quality={100}
+            />
+
+            <div className="absolute right-10 bottom-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="truncate text-sm">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0
+                })}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
